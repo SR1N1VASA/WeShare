73d4b20f74c26629efe41bf893c4a70c87078a6c
SR1N1VASA <srinivasayachitula@gmail.com>
2021-06-07 01:10:04 -0400

    adding mongoDB as database

--- WeShare/backend/app.js
+++ WeShare/backend/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
+
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://sri:r1YWhlbFyFNkC6Eh@weshare.3leqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,29 +36,31 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
   res.status(201).json({
-    message: 'Post added successfully'
+    message: 'Post added successfully',
+    postId: createdPost._id
+  });
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
+  Post.find().then(documents => {
   res.status(200).json({
     message: "Posts fetched successfully!",
-    posts: posts
+    posts: documents
+  });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
   });
 });
 
